6f608673862438a9f153edf490c5ef48b163adca
bhargavkathivarapu <bhargavkathivarapu31@gmail.com>
2016-01-25 13:16:13 +0530

    P3
    run as popen by server

---
+++ allinone/pofd.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<signal.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+int main(int argc,char* argv[])
+{
+	//char buffer[100];int n=0;
+	
+	
+	printf("namasthe\n");
+	
+	
+}
